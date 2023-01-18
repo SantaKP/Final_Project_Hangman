4947602fed82ce96c970c0927264dd831aa26d23
Santa Kesane <s.plenkova@gmail.com>
2023-01-18 13:50:01 +0200

    tryout

---
+++ Hangman_body/Hangman_body/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace Drawing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int mistakes = 10;
+
+
+            if (mistakes == 2)
+
+            {
+                for (int i = 0; i < 11; i++)
+                    Console.WriteLine("|");
+                Console.WriteLine("|");
+                Console.WriteLine("|");
+                Console.WriteLine("|");
+                Console.WriteLine("|");
+                Console.WriteLine("|");
+            }
+            if (mistakes == 3)
+            {
+                Console.WriteLine("|             |");
+                Console.WriteLine("|             |");
+                Console.WriteLine("|             |");
+                Console.WriteLine("|             |");
+                Console.WriteLine("|             |");
+                Console.WriteLine("|             |");
+
+                for (int i = 0; i < 9; i++) ;
+                   
+
+            }
+
+            if (mistakes == 4)
+            {
+                Console.WriteLine("|-------------|");
+                Console.WriteLine("|             |");
+                Console.WriteLine("|             |");
+                Console.WriteLine("|             |");
+                Console.WriteLine("|             |");
+                Console.WriteLine("|             |");
+
+
+                for (int i = 0; i < 8; i++) ;
+                    
+
+            }
+
+            if (mistakes == 5)
+            {
+                Console.WriteLine("|------|-------|");
+                Console.WriteLine("|      |       |");
+                Console.WriteLine("|              |");
+                Console.WriteLine("|              |");
+                Console.WriteLine("|              |");
+                Console.WriteLine("|              |");
+
+                for (int i = 0; i < 7; i++) ;
+                   
+                
+
+            }
+
+            if (mistakes == 6)
+            {
+                Console.WriteLine("|------|-------|");
+                Console.WriteLine("|      |       |");
+                Console.WriteLine("|      o       |");
+                Console.WriteLine("|              |");
+                Console.WriteLine("|              |");
+                Console.WriteLine("|              |");
+
+                for (int i = 0; i < 6; i++)
+                    Console.WriteLine("    ||");
+                Console.WriteLine("-----------------------");
+
+            }
+
+            if (mistakes == 7)
+            {
+                Console.WriteLine("|------|-------|");
+                Console.WriteLine("|      |       |");
+                Console.WriteLine("|      o       |");
+                Console.WriteLine("|      |       |");
+                Console.WriteLine("|              |");
+                Console.WriteLine("|              |");
+
+                for (int i = 0; i < 7; i++) ;
+
+
+
+
+            }
+
+
+            if (mistakes == 8)
+            {
+                Console.WriteLine("|------|-------|");
+                Console.WriteLine("|      |       |");
+                Console.WriteLine("|      o       |");
+                Console.WriteLine("|     -|       |");
+                Console.WriteLine("|              |");
+                Console.WriteLine("|              |");
+
+                for (int i = 0; i < 8; i++) ;
+
+
+
+
+            }
+
+            if (mistakes == 9)
+            {
+                Console.WriteLine("|------|-------|");
+                Console.WriteLine("|      |       |");
+                Console.WriteLine("|      o       |");
+                Console.WriteLine("|     -|-      |");
+                Console.WriteLine("|              |");
+                Console.WriteLine("|              |");
+
+                for (int i = 0; i < 7; i++) ;
+
+
+
+
+            }
+            if (mistakes == 10)
+            {
+                Console.WriteLine("|------|-------|");
+                Console.WriteLine("|      |       |");
+                Console.WriteLine("|      o       |");
+                Console.WriteLine("|     -|-      |");
+                Console.WriteLine("|      ^       |");
+                Console.WriteLine("|              |");
+
+                for (int i = 0; i < 7; i++) ;
+
+
+
+
+            }
+        }
+    }
+}
---
+++ Hangman_template/Hangman_template/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+using System.Transactions;
+
+class Hangman_template
+{
+
+    static void Main(string[] args) { 
+
+        // i left animal names in bracket so we would know, when finish we can take them out.
+
+    string[] clue = { "Funny videos (cat)" , "not welcome  in house (rat)", "Tom`s favorite kind (mouse)", "Wild and free (horse)", "Never drinks warm coffee (giraffe)", "fast and fearful (rabbit)", "Majestic (deer)", "Can`t jump (elephant)", "No gray zone(zebra)", "Jups and swims (dolphin)" };
+    int[] numb = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+        string clueNumb;
+        Console.WriteLine("Please enter number 1-10:");
+       
+        clueNumb = (clue[int.Parse(Console.ReadLine())]);
+        Console.WriteLine("Your clue is:");
+        Console.WriteLine(clueNumb);
+
+        Console.WriteLine("The game starts now!");
+
+        Console.WriteLine("Please enter your first guess: ");
+
+        Console.ReadLine();
+
+
+
+       
+
+
+
+
+
+
+    }
+
+  
+    
+
+
+
+
+
+
+}
